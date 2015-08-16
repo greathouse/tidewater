b6d3321fe473baf52795e0a8c31cf0ac55e8ef39
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-08-15 22:58:57 -0500

    Refactor

--- runtime/src/main/groovy/greenmoonsoftware/tidewater/runtime/Start.groovy
+++ runtime/src/main/groovy/greenmoonsoftware/tidewater/runtime/Start.groovy
@@ -2,14 +2,11 @@ package greenmoonsoftware.tidewater.runtime
 
 import greenmoonsoftware.tidewater.config.Context
 import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
 
 class Start {
     static void main(String[] args) {
         new Start().start('''
-            import greenmoonsoftware.tidewater.scm.git.*
-            import greenmoonsoftware.tidewater.gradle.*
-            import greenmoonsoftware.tidewater.shell.*
-
             step checkout(type: GitClone) {
                 url 'https://github.com/greathouse/green-tea-test.git'
                 dir 'green-tea-test'
@@ -27,25 +24,48 @@ class Start {
             }
 
             step shell(type: Shell) {
-                contents """
+                contents """\
                     #!/bin/bash
                     echo hello from the shell
                     whoami
-                """
+                """.stripIndent()
             }
         ''')
     }
 
     public void start(String script) {
-        Binding binding = new Binding()
-        CompilerConfiguration cc = new CompilerConfiguration();
-        cc.setScriptBaseClass(DelegatingScript.class.getName());
-        GroovyShell shell = new GroovyShell(this.class.classLoader, binding, cc)
-        DelegatingScript script1 = (DelegatingScript)shell.parse(script)
-
-        script1.setDelegate(Context.get())
+        DelegatingScript script1 = setupScript(script)
         script1.run()
 
         Context.get().execute()
+    }
+
+    private DelegatingScript setupScript(String script) {
+        DelegatingScript script1 = parseScript(setupCompilerConfiguration(), script)
+        script1.setDelegate(Context.get())
+        return script1
+    }
+
+    private CompilerConfiguration setupCompilerConfiguration() {
+        CompilerConfiguration cc = new CompilerConfiguration();
+        cc.setScriptBaseClass(DelegatingScript.class.getName());
+        addImports(cc)
+        return cc
+    }
+
+    private DelegatingScript parseScript(CompilerConfiguration cc, String script) {
+        GroovyShell shell = new GroovyShell(this.class.classLoader, new Binding(), cc)
+        DelegatingScript script1 = (DelegatingScript) shell.parse(script)
+        return script1
+    }
+
+    private void addImports(CompilerConfiguration cc) {
+        def imports = new ImportCustomizer()
+        imports.addStarImports(
+                'greenmoonsoftware.tidewater.scm.git',
+                'greenmoonsoftware.tidewater.gradle',
+                'greenmoonsoftware.tidewater.shell'
+        )
+        cc.addCompilationCustomizers imports
     }
 }
