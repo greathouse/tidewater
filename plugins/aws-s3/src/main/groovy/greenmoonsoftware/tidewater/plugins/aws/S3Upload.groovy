f7dc298d5ad8e627af434a6ac8e6a79d92ea23d4
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-09-09 22:25:44 -0500

    Refactor/clean-up of S3Upload step

--- plugins/aws-s3/src/main/groovy/greenmoonsoftware/tidewater/plugins/aws/S3Upload.groovy
+++ plugins/aws-s3/src/main/groovy/greenmoonsoftware/tidewater/plugins/aws/S3Upload.groovy
@@ -1,12 +1,13 @@
 package greenmoonsoftware.tidewater.plugins.aws
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.services.s3.transfer.Upload
 import greenmoonsoftware.tidewater.config.Context
 import greenmoonsoftware.tidewater.step.AbstractStep
 import greenmoonsoftware.tidewater.step.Input
+
+import java.text.DecimalFormat
 
 class S3Upload extends AbstractStep {
     @Input String bucketName
@@ -16,41 +17,49 @@ class S3Upload extends AbstractStep {
     @Override
     boolean execute(Context context, File stepDirectory) {
         def log = context.&log.curry(this)
-        def s3client = new AmazonS3Client(new ProfileCredentialsProvider())
-        File file = new File(upload)
-        if (file.isDirectory()) {
-            uploadDirectory(log, s3client, file)
-        }
-        else {
-            uploadSingleFile(log, s3client, file)
-        }
-        return true
+        File file = new File(context.workspace, upload)
+        return (file.isDirectory()) ? uploadDirectory(log, file) : uploadSingleFile(log, file)
     }
 
-    private void uploadDirectory(Closure log, AmazonS3 s3client, File directory) {
+    private boolean uploadDirectory(Closure log, File directory) {
         log "Uploading directory ${directory.absolutePath}"
+        return upload(log) { directoryTransferUtility ->
+            directoryTransferUtility.uploadDirectory(bucketName, keyName ?: '', directory, true)
+        }
+    }
+
+    private boolean uploadSingleFile(Closure log, File file) {
+        log "Uploading file: ${file.absolutePath}"
+        return upload(log) { directoryTransferUtility ->
+            directoryTransferUtility.upload(bucketName, keyName ?: '', file)
+        }
+    }
+
+    private boolean upload(Closure log, Closure<Upload> withTransferUtility) {
+        def s3client = new AmazonS3Client(new ProfileCredentialsProvider())
         def directoryTransferUtility = new TransferManager(s3client)
-        def upload = directoryTransferUtility.uploadDirectory(bucketName, keyName ?: '', directory, true)
+        def upload = withTransferUtility(directoryTransferUtility)
         monitorUpload(upload, log)
         upload.waitForCompletion()
         directoryTransferUtility.shutdownNow()
         log 'Upload successful'
+        return true
     }
 
     private monitorUpload(upload, Closure log) {
         new Thread({
             while (!upload.done) {
                 def progress = upload.progress
-                log "${progress.percentTransferred as int}% (${progress.bytesTransferred} / ${progress.totalBytesToTransfer} bytes)"
+                log "${progress.percentTransferred as int}% (${readableFileSize(progress.bytesTransferred)} / ${readableFileSize(progress.totalBytesToTransfer)} bytes)"
                 sleep 2000
             }
         }).start()
     }
 
-    private void uploadSingleFile(Closure log, AmazonS3Client s3client, File file) {
-        log "Uploading file: ${file.absolutePath}"
-        s3client.putObject(new PutObjectRequest(bucketName, keyName ?: file.name, file))
-        log 'Upload successful'
+    private String readableFileSize(long size) {
+        if(size <= 0) return '0'
+        final String[] units = [ 'B', 'kB', 'MB', 'GB', 'TB' ] as String[]
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024))
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups]
     }
-
 }
