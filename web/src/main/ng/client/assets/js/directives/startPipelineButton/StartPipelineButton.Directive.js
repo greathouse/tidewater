0f38fcea9539b33d7f1ab57704a3ddb78952882b
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-10-19 21:46:08 -0500

    Add spinners and status images to start-pipeline-button directive

--- web/src/main/ng/client/assets/js/directives/startPipelineButton/StartPipelineButton.Directive.js
+++ web/src/main/ng/client/assets/js/directives/startPipelineButton/StartPipelineButton.Directive.js
@@ -3,17 +3,26 @@ var m = angular.module('startPipelineButton', []);
 m.directive('startPipelineButton', function() {
     var d = {
         scope: {
-            pipelineName: '=',
+            pipelineName: '='
         },
         restrict: 'E',
         templateUrl: '/templates/directives/startPipelineButton/template.html',
-        controller: ['$scope', '$http', function($scope, $http) {
+        controller: ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+            $scope.idle = function() {
+                return !$scope.loading && !$scope.success && !$scope.error;
+            };
             $scope.start = function() {
+                $scope.loading = true;
                 $http.get('/pipelines/' + $scope.pipelineName + '/start')
                 .then (
                     function (response) {
+                        $scope.loading = false;
+                        $scope.success = true;
+                        $timeout(function() {$scope.success = false;}, 3000)
                     },
                     function(response) {
+                        $scope.loading = false;
+                        $scope.error = true;
                     }
                 );
             }
