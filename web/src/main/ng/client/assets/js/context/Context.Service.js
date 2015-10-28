e5e27369d579f21fc6baaf8a81d08ded0851d8e6
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-10-28 15:55:30 -0500

    Kill if statements

--- web/src/main/ng/client/assets/js/context/Context.Service.js
+++ web/src/main/ng/client/assets/js/context/Context.Service.js
@@ -2,6 +2,62 @@ angular.module('contextModule').factory('Context.Service', ['$http',
 
 function($http) {
     var contexts = {};
+    var eventHandlers = {
+        'greenmoonsoftware.tidewater.step.events.StepDefinedEvent': function(event) {
+            var context = contexts[event.contextId.id];
+            context.steps[event.name] = {
+                stepName: event.name,
+                stepType: event.stepType,
+                attempts: []
+            };
+        },
+        'greenmoonsoftware.tidewater.context.events.ContextExecutionStartedEvent': function(event) {
+            var context = contexts[event.aggregateId];
+            context.script = event.script;
+            context.startTime = event.eventDateTime.epochSecond;
+            context.workspace = event.workspace;
+            context.metaDirectory = event.metaDirectory;
+            context.status = 'IN_PROGRESS';
+        },
+        'greenmoonsoftware.tidewater.step.events.StepConfiguredEvent': function(event) {
+            var context = contexts[event.contextId.id];
+            context.steps[event.aggregateId].inputs = event.step.inputs;
+        },
+        'greenmoonsoftware.tidewater.step.events.StepStartedEvent': function(event) {
+            var context = contexts[event.contextId.id];
+            var step = context.steps[event.aggregateId];
+            step.attempts.push({
+                status: 'IN_PROGRESS',
+                startTime: event.eventDateTime.epochSecond,
+                endTime: 0,
+                logs: []
+            });
+        },
+        'greenmoonsoftware.tidewater.step.events.StepLogEvent': function(event) {
+            var context = contexts[event.contextId.id];
+            var attempt = context.steps[event.aggregateId].attempts.slice(-1)[0];
+            attempt.logs.push({
+                time: event.eventDateTime.epochSecond,
+                message: event.message
+            })
+        },
+        'greenmoonsoftware.tidewater.step.events.StepSuccessfullyCompletedEvent': function(event) {
+            var context = contexts[event.contextId.id];
+            var step = context.steps[event.aggregateId];
+            context.lastCompletedStep = step;
+            step.outputs = event.step.outputs;
+            var attempt = step.attempts.slice(-1)[0];
+            attempt.status = 'SUCCESS';
+            attempt.endTime = event.endDate;
+            attempt.duration = attempt.endTime - attempt.startTime;
+        },
+        'greenmoonsoftware.tidewater.context.events.ContextExecutionEndedEvent': function(event) {
+            var context = contexts[event.aggregateId];
+            context.endTime = event.eventDateTime.epochSecond;
+            var lastAttempt = context.lastCompletedStep.attempts.slice(-1)[0];
+            context.status = lastAttempt.status;
+        }
+    }
 
     function getContext(contextId) {
         var context = contexts[contextId];
@@ -31,59 +87,8 @@ function($http) {
     };
 
     function processEvent(event, index) {
-        if (event.type === 'greenmoonsoftware.tidewater.step.events.StepDefinedEvent') {
-            var context = contexts[event.contextId.id];
-            context.steps[event.name] = {
-                stepName: event.name,
-                stepType: event.stepType,
-                attempts: []
-            };
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.context.events.ContextExecutionStartedEvent') {
-            var context = contexts[event.aggregateId];
-            context.script = event.script;
-            context.startTime = event.eventDateTime.epochSecond;
-            context.workspace = event.workspace;
-            context.metaDirectory = event.metaDirectory;
-            context.status = 'IN_PROGRESS';
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.step.events.StepConfiguredEvent') {
-            var context = contexts[event.contextId.id];
-            context.steps[event.aggregateId].inputs = event.step.inputs;
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.step.events.StepStartedEvent') {
-            var context = contexts[event.contextId.id];
-            var step = context.steps[event.aggregateId];
-            step.attempts.push({
-                status: 'IN_PROGRESS',
-                startTime: event.eventDateTime.epochSecond,
-                endTime: 0,
-                logs: []
-            });
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.step.events.StepLogEvent') {
-            var context = contexts[event.contextId.id];
-            var attempt = context.steps[event.aggregateId].attempts.slice(-1)[0];
-            attempt.logs.push({
-                time: event.eventDateTime.epochSecond,
-                message: event.message
-            })
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.step.events.StepSuccessfullyCompletedEvent') {
-            var context = contexts[event.contextId.id];
-            var step = context.steps[event.aggregateId];
-            context.lastCompletedStep = step;
-            step.outputs = event.step.outputs;
-            var attempt = step.attempts.slice(-1)[0];
-            attempt.status = 'SUCCESS';
-            attempt.endTime = event.endDate;
-            attempt.duration = attempt.endTime - attempt.startTime;
-        }
-        else if (event.type === 'greenmoonsoftware.tidewater.context.events.ContextExecutionEndedEvent') {
-            var context = contexts[event.aggregateId];
-            context.endTime = event.eventDateTime.epochSecond;
-            var lastAttempt = context.lastCompletedStep.attempts.slice(-1)[0];
-            context.status = lastAttempt.status;
+        if (eventHandlers.hasOwnProperty(event.type)) {
+            eventHandlers[event.type](event);
         }
     };
 
