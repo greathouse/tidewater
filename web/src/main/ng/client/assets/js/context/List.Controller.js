dbe77896c2233f3ae06e539a4c3a50cbee778b28
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-10-31 21:39:36 -0500

    Add context to list upon start event

--- web/src/main/ng/client/assets/js/context/List.Controller.js
+++ web/src/main/ng/client/assets/js/context/List.Controller.js
@@ -13,7 +13,16 @@ function ($scope, $http, $filter, $routeParams, foundationApi, eventService) {
   eventService.register('Context.ListController', processEvent);
 
   function processEvent(event) {
-      if (event.type === 'greenmoonsoftware.tidewater.web.context.events.PipelineContextEndedEvent') {
+      if (event.type === 'greenmoonsoftware.tidewater.web.context.events.PipelineContextStartedEvent') {
+        $scope.contexts.unshift({
+          contextId: event.aggregateId,
+          pipelineName: $routeParams.pipelineName,
+          status: 'IN_PROGRESS',
+          startTime: event.eventDateTime.epochSecond * 1000,
+        });
+        $scope.apply();
+      }
+      else if (event.type === 'greenmoonsoftware.tidewater.web.context.events.PipelineContextEndedEvent') {
         var c = $filter('getBy')(self.contexts, 'contextId', event.aggregateId);
         c.status = event.status;
         c.endTime = event.endTime.epochSecond * 1000;
