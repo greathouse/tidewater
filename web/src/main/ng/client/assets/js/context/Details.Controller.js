052c5d9669a08908883030cd9997174e2c9e6429
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-02 21:20:25 -0600

    Add input/output to steps in UI

--- web/src/main/ng/client/assets/js/context/Details.Controller.js
+++ web/src/main/ng/client/assets/js/context/Details.Controller.js
@@ -7,6 +7,11 @@ function ($scope, $routeParams, contextService) {
   $scope.showScript = true;
   $scope.pipelineName = $routeParams.pipelineName;
 
+  $scope.isEmpty = function(object) {
+    var v = Object.keys(object).length;
+    return v == 0;
+  }
+
   $scope.$watch(
     function() { return contextService.getContext($routeParams.contextId); },
     function(newVal) { $scope.context = newVal; },
