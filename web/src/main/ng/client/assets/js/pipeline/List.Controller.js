d8592d03e1fed61bf506219eec36b5b54a24e0c7
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-03 22:15:34 -0600

    Add Pipeline.Service javascript module

--- web/src/main/ng/client/assets/js/pipeline/List.Controller.js
+++ web/src/main/ng/client/assets/js/pipeline/List.Controller.js
@@ -1,35 +1,15 @@
 angular.module('pipelineModule').controller('Pipeline.ListController', [
     '$scope',
-    '$http',
     '$filter',
-    'FoundationApi',
-    'Event.Service',
+    'Pipeline.Service',
 
-function ($scope, $http, $filter, foundationApi, eventService) {
-  eventService.register('Pipeline.ListController', processEvent);
-
-  var self = this;
-  var pipelines;
-
-  function processEvent(event) {
-      if (event.type === 'greenmoonsoftware.tidewater.web.context.events.PipelineContextStartedEvent') {
-          var pipeline = $filter('getBy')(self.pipelines, 'name', event.pipelineName);
-          pipeline.status = 'IN_PROGRESS';
-          $scope.$apply();
-      }
-      if (event.type === 'greenmoonsoftware.tidewater.web.context.events.PipelineContextEndedEvent') {
-        var pipeline = $filter('getBy')(self.pipelines, 'name', event.pipelineName);
-        pipeline.status = 'COMPLETED';
-        $scope.$apply();
-      }
-  };
-
-  $http.get('/pipelines').
-    then (function (response) {
-      self.pipelines = response.data;
-      $scope.pipelines = self.pipelines;
-    }, function(response) {
-      foundationApi.publish('main-notifications', { color: 'alert', autoclose: 3000, content: 'Failed' });
-    });
+function ($scope, $filter, pipelineService) {
+    $scope.$watch(
+        function() { return pipelineService.getList(); },
+        function(newVal) {
+            $scope.pipelines = Object.keys(newVal).map(key => newVal[key]);
+        },
+        true
+    );
 }
 ]);
--- web/src/main/ng/client/assets/js/pipeline/Pipeline.Service.js
+++ web/src/main/ng/client/assets/js/pipeline/Pipeline.Service.js
@@ -1,11 +1,20 @@
-angular.module('pipelineModule').factory('Pipeline.Service', ['$rootScope', '$http', 'Event.Service',
+angular.module('pipelineModule').factory('Pipeline.Service', [
+    '$rootScope',
+    '$http',
+    'FoundationApi',
+    'Event.Service',
 
-function($rootScope, $http, eventService) {
+function($rootScope, $http, foundationApi, eventService) {
     eventService.register('Context.Service', processEvent);
 
     var pipelines = {};
     var eventHandlers = {
-
+        'greenmoonsoftware.tidewater.web.pipeline.events.PipelineCreatedEvent': function(event) {
+            pipelines[event.aggregateId] = {
+                name: event.aggregateId,
+                script: event.scrip
+            };
+        }
     };
 
     function processEvent(event, index) {
@@ -14,6 +23,31 @@ function($rootScope, $http, eventService) {
             $rootScope.$apply();
         }
     };
+
+    function loadPipelines() {
+        $http.get('/pipelines').
+        then (
+            function (response) {
+                angular.forEach(response.data, function(pipeline) {
+                    pipelines[pipeline.name] = pipeline;
+                });
+            },
+            function(response) {
+                foundationApi.publish('main-notifications', { color: 'alert', autoclose: 3000, content: 'Failed to load pipelines' });
+            }
+        );
+    };
+
+    function getList() {
+        if (Object.keys(pipelines).length === 0) {
+            loadPipelines();
+        }
+        return pipelines;
+    };
+
+    return {
+        getList: getList
+    }
 }
 
 ]);
