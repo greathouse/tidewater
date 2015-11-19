4bc47d36f9f841731db352a9018e1e6e33f60ed8
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-18 21:58:51 -0600

    Attempt to implement custom progressbar, but was thwarted by package scoped variables

--- plugins/docker/src/main/groovy/greenmoonsoftware/tidewater/plugins/docker/pull/TidewaterPullImageResultCallback.groovy
+++ plugins/docker/src/main/groovy/greenmoonsoftware/tidewater/plugins/docker/pull/TidewaterPullImageResultCallback.groovy
@@ -1,6 +1,9 @@
 package greenmoonsoftware.tidewater.plugins.docker.pull
 import com.github.dockerjava.api.model.PullResponseItem
+import com.github.dockerjava.api.model.ResponseItem.ProgressDetail
 import com.github.dockerjava.core.async.ResultCallbackTemplate
+
+import java.text.DecimalFormat
 
 class TidewaterPullImageResultCallback extends ResultCallbackTemplate<TidewaterPullImageResultCallback, PullResponseItem> {
     private final Closure log
@@ -24,19 +27,41 @@ class TidewaterPullImageResultCallback extends ResultCallbackTemplate<TidewaterP
 
     @Override
     void onError(Throwable throwable) {
+        throwable.printStackTrace()
         success = false
         log stringifyThrowable(throwable)
+        try {
+            close();
+        } catch (IOException e) {
+            //ignore
+        }
     }
 
     @Override
     void onNext(PullResponseItem item) {
-        log item.id, "${item.status} ${item.progress ?: ''}"
+        log item.id, "${item.status} ${item.progressDetail ? buildProgressBar(item) : ''}"
     }
 
-    private StringWriter stringifyThrowable(Throwable throwable) {
+    private String stringifyThrowable(Throwable throwable) {
         def sw = new StringWriter()
         def pw = new PrintWriter(sw)
         throwable.printStackTrace(pw)
         sw.toString()
+    }
+
+    private String buildProgressBar(PullResponseItem i) {
+        return i.progress //Unfortunately the attributes in the progressDetails class are package scoped so we must use the deprecated method to get a progress bar.
+//        def d = i.progressDetail
+//        def percentageScale = (d.current / d.total) / 2
+//        def bar = '' * percentageScale
+//        bar = bar.padRight(50 - percentageScale, ' ')
+//        return "[>${bar}]  ${byteSizeToHuman(d.current)}/${byteSizeToHuman(d.total)}"
+    }
+
+    private String byteSizeToHuman(long size) {
+        if(size <= 0) return '0'
+        final String[] units = [ 'B', 'kB', 'MB', 'GB', 'TB' ] as String[]
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024))
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups]
     }
 }
