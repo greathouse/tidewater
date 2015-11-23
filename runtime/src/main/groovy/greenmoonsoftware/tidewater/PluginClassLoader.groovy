b00c5b9bcc500d1e0874c80906924800d112bca3
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-22 22:13:50 -0600

    Clean-up PluginClassLoader

--- runtime/src/main/groovy/greenmoonsoftware/tidewater/PluginClassLoader.groovy
+++ runtime/src/main/groovy/greenmoonsoftware/tidewater/PluginClassLoader.groovy
@@ -1,56 +1,55 @@
 package greenmoonsoftware.tidewater
 
+//https://dzone.com/articles/java-classloader-handling
 public class PluginClassLoader extends ClassLoader {
-    private ChildClassLoader childClassLoader;
+    private final ChildClassLoader childClassLoader
 
     public PluginClassLoader() {
-        super(Thread.currentThread().getContextClassLoader());
+        super(Thread.currentThread().getContextClassLoader())
         def uris = new File('/Users/robert/projects/tidewater/web/build/plugins')
                 .listFiles()
                 .collect { it.toURI().toURL() } as URL[]
-        childClassLoader = new ChildClassLoader(uris, new DetectClass(this.getParent()));
+        childClassLoader = new ChildClassLoader(uris, new DetectClass(this.getParent()))
     }
 
     @Override
     protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
         try {
-            return childClassLoader.findClass(name);
+            return childClassLoader.findClass(name)
         }
         catch (ClassNotFoundException e) {
-            return super.loadClass(name, resolve);
+            return super.loadClass(name, resolve)
         }
     }
 
     private static class ChildClassLoader extends URLClassLoader {
-        private DetectClass realParent;
+        private final DetectClass realParent
 
         public ChildClassLoader(URL[] urls, DetectClass realParent) {
-            super(urls, (ClassLoader) null);
-            this.realParent = realParent;
+            super(urls, (ClassLoader) null)
+            this.realParent = realParent
         }
 
         @Override
         public Class<?> findClass(String name) throws ClassNotFoundException {
             try {
-                Class<?> loaded = super.findLoadedClass(name);
-                if (loaded != null)
-                    return loaded;
-                return super.findClass(name);
+                def loaded = super.findLoadedClass(name)
+                return loaded ?: super.findClass(name)
             }
             catch (ClassNotFoundException e) {
-                return realParent.loadClass(name);
+                return realParent.loadClass(name)
             }
         }
     }
 
     private static class DetectClass extends ClassLoader {
         public DetectClass(ClassLoader parent) {
-            super(parent);
+            super(parent)
         }
 
         @Override
         public Class<?> findClass(String name) throws ClassNotFoundException {
-            return super.findClass(name);
+            return super.findClass(name)
         }
     }
 }
