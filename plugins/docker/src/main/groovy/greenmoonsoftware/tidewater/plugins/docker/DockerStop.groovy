9b9f753261eaf1dc2dba7d250ef36e8e1ed38594
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-25 13:50:56 -0600

    Add DockerStop step

---
+++ plugins/docker/src/main/groovy/greenmoonsoftware/tidewater/plugins/docker/DockerStop.groovy
@@ -0,0 +1,43 @@
+package greenmoonsoftware.tidewater.plugins.docker
+
+import com.github.dockerjava.api.DockerClient
+import greenmoonsoftware.tidewater.Context
+import greenmoonsoftware.tidewater.step.AbstractStep
+import greenmoonsoftware.tidewater.step.Input
+import groovy.json.JsonOutput
+
+import static greenmoonsoftware.tidewater.plugins.docker.Client.dockerClient
+
+class DockerStop extends AbstractStep {
+    @Input String containerId
+    @Input String uri = System.env['DOCKER_HOST']
+    @Input String certPath = System.env['DOCKER_CERT_PATH']
+
+    private Closure<Void> log
+    private DockerClient docker
+
+    @Override
+    boolean execute(Context context, File stepDirectory) {
+        init(context)
+        stop()
+        return isStopped()
+    }
+
+    private void init(Context context) {
+        log = context.&log.curry(this)
+        docker = dockerClient(uri, certPath)
+    }
+
+    private void stop() {
+        log "Attempting to stop container: ${containerId}"
+        docker.stopContainerCmd(containerId).exec()
+    }
+
+    private boolean isStopped() {
+        log 'Inspecting...'
+        def inspect = docker.inspectContainerCmd(containerId).exec()
+        log JsonOutput.prettyPrint(JsonOutput.toJson(inspect))
+
+        return !inspect.state.isRunning()
+    }
+}
