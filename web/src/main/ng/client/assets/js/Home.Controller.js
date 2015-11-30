fdfa2d8400703b5dddf78f2fe015319e3c9751ba
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-30 00:27:58 -0600

    Add git-gradle sample

--- web/src/main/ng/client/assets/js/Home.Controller.js
+++ web/src/main/ng/client/assets/js/Home.Controller.js
@@ -2,7 +2,8 @@ angular.module('application').controller('Application.HomeController', ['$scope'
 
 function($scope) {
     $scope.samples = [
-        { title: 'Docker', template: '/templates/samples/docker.html' }
+        { title: 'Docker', template: '/templates/samples/docker.html' },
+        { title: 'Git / Gradle', template: '/templates/sample/git-gradle.html' }
     ];
 }
 
