933aa2d3948ec25e180f52a4bb79767933a9a17b
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-11-29 23:44:48 -0600

    Add DockerTag step

---
+++ plugins/docker/src/main/groovy/greenmoonsoftware/tidewater/plugins/docker/DockerTag.groovy
@@ -0,0 +1,32 @@
+package greenmoonsoftware.tidewater.plugins.docker
+
+import com.github.dockerjava.api.DockerClient
+import greenmoonsoftware.tidewater.Context
+import greenmoonsoftware.tidewater.step.AbstractStep
+import greenmoonsoftware.tidewater.step.Input
+
+class DockerTag extends AbstractStep {
+    @Input String uri = System.env['DOCKER_HOST']
+    @Input String certPath = System.env['DOCKER_CERT_PATH']
+    @Input String imageToTag
+    @Input String repository
+    @Input String tag
+
+    private Closure<Void> log
+    private DockerClient docker
+
+    @Override
+    boolean execute(Context context, File stepDirectory) {
+        init(context)
+
+        log "Attempting to tag ${imageToTag} → ${repository}:${tag}"
+        docker.tagImageCmd(imageToTag, repository, tag).exec()
+        log 'Successful'
+        return true
+    }
+
+    private init(Context context) {
+        log = context.&log.curry(this)
+        docker = Client.dockerClient(uri, certPath)
+    }
+}
