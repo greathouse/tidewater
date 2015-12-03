31728d39caf0d138b942c5a7b00c8d9f0fc75bfc
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-12-02 22:11:13 -0600

    Add another sample script to the Home page

--- web/src/main/ng/client/assets/js/Home.Controller.js
+++ web/src/main/ng/client/assets/js/Home.Controller.js
@@ -3,7 +3,8 @@ angular.module('application').controller('Application.HomeController', ['$scope'
 function($scope) {
     $scope.samples = [
         { title: 'Docker', template: '/templates/samples/docker.html' },
-        { title: 'Git / Gradle', template: '/templates/samples/git-gradle.html' }
+        { title: 'Git / Gradle', template: '/templates/samples/git-gradle.html' },
+        { title: 'Backup, Create, and Publish Static Site to AWS S3', template: '/templates/samples/s3-docker.html' }
     ];
 }
 
