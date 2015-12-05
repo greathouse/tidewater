10dbd8cc010cbf2a1eeff3001f25e7d71274976b
Robert G. Greathouse <greathouse.robert@gmail.com>
2015-12-04 20:35:49 -0600

    Redirect successful pipeline creations to pipeline view

--- web/src/main/ng/client/assets/js/pipeline/New.Controller.js
+++ web/src/main/ng/client/assets/js/pipeline/New.Controller.js
@@ -1,10 +1,11 @@
-angular.module('pipelineModule').controller('Pipeline.NewController', ['$scope', '$http', 'FoundationApi',
+angular.module('pipelineModule').controller('Pipeline.NewController', ['$scope', '$http', 'FoundationApi', '$location',
 
-function ($scope, $http, foundationApi) {
+function ($scope, $http, foundationApi, $location) {
   $scope.postScript = function(formData) {
     $http.post('/pipeline', formData).
       then (function (response) {
         foundationApi.publish('main-notifications', { color: 'success', autoclose: 3000, content: 'Pipeline Created' });
+        $location.path('/pipelines/' + formData.name);
       }, function(response) {
         foundationApi.publish('main-notifications', { color: 'alert', autoclose: 3000, content: 'Failed' });
       });
